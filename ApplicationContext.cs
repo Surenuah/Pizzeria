e9b24ecf6bd91e4e7c5f3824ab06caec1b7911c2
Surenuah <kaliroseswiththegoldengun@gmail.com>
2021-04-14 12:44:56 +0600

    Pizzeria
    Module 06

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace WpfApp2
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationContext() : base("Pizzeria.db")
+        {
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int User_Id { get; set; }
+        public int Product_Id { get; set; }
+    }
+}
